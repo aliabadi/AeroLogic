3ecc9865b92e3b8ae40baf1b96da126b4a77971f
Nicholas Aliabadi <aliabadi@users.noreply.github.com>
2020-03-16 18:42:12 -0700

    Delete AeroConfig.h

--- AeroLogic/AeroConfig.h
+++
@@ -1,9 +0,0 @@
-//how many clients should be able to telnet to this ESP32
-//#define MAX_SRV_CLIENTS 1
-
-const char* ssid = "FBI Surveillance Van";
-const char* password = "quantumsupremacy";
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = -28800;
-const int   daylightOffset_sec = 3600;
